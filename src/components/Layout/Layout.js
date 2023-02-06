107232b0e072eddd5858c8638d9e59c8f1257a42
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2023-02-06 19:13:02 -0300

    add login and favorites component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,26 @@
 import { useState } from "react";
-import { Login } from "../../Views/Login";
+
+import { Login } from "../Login/Login";
 import { Footer } from "../Footer/Footer";
 import { Header } from "../Header/Header";
 
 import { SideMenu } from "./SideMenu";
+import { Favorites } from "../Favorites/Favorites";
 
 export const Layout = ({ children }) => {
- const [isOpenMenu, setIsOpenMenu] = useState(false)
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+ 
+  const [isFavorite, setIsFavorite] = useState(false);
+ 
 
   return (
     <>
-      <Header setIsOpenMenu={setIsOpenMenu} />
+      <Header setIsOpenMenu={setIsOpenMenu} setShowLogin={setShowLogin} setIsFavorite={setIsFavorite} />
       <SideMenu setIsOpenMenu={setIsOpenMenu} isOpenMenu={isOpenMenu} />
       {children}
-      
+      <Login showLogin={showLogin} setShowLogin={setShowLogin} />
+      <Favorites isFavorite={isFavorite} setIsFavorite={setIsFavorite} />
       <Footer />
     </>
   );

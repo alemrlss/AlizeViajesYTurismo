ee7e4a234e86ef133281fdab5d86cef4f7d7dfe2
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2023-02-06 19:13:50 -0300

    add login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,6 +4,7 @@ import "./Login.css";
 
 export const Login = ({ showLogin, setShowLogin }) => {
   const [display, setDisplay] = useState("flex");
+  const [isRegister, setIsRegister] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
@@ -14,6 +15,7 @@ export const Login = ({ showLogin, setShowLogin }) => {
   return (
     <div
       style={{ display: display }}
+      // onClick={() => setShowLogin(false)}
       className={showLogin ? "login" : "login login--open login--out"}
     >
       <div
@@ -23,45 +25,98 @@ export const Login = ({ showLogin, setShowLogin }) => {
             : " login__container login__container--out "
         }
       >
-        <div className="login__header">
-          <TfiClose
-            className="login__icon"
-            onClick={() => setShowLogin(false)}
-          />
-        </div>
-        <div className="login__card">
-          <div className="login__form">
-            <form className="login__email" action="">
-              <h1 className="login__title">Entra o registrate en Alize</h1>
-              <div className="login__input-wrapper">
-                <input
-                  className="login__input"
-                  placeholder="Email"
-                  type="email"
-                />
-              </div>
-              <button className="login__btn">
-                <span className="login__btn-span">Continuar</span>
-              </button>
-            </form>
-            <div className="login__middle"></div>
-            <div className="login__access">
-              <button className="login__access-btn">
-                <span className="login__access-icon login__access-icon--google"></span>
-                <p className="login__acccess-p">Continuar con Google</p>
-              </button>
-              <button className="login__access-btn login__access-btn--apple">
-                <span className="login__access-icon login__access-icon--apple"></span>
-                <p className="login__acccess-p">Continuar con Apple</p>
-              </button>
-              <button className="login__access-btn">
-                <span className="login__access-icon login__access-icon--facebook"></span>
-                <p className="login__acccess-p">Continuar con Facebook</p>
-              </button>
+        {isRegister ? (
+          <RegisterComponent setShowLogin={setShowLogin} />
+        ) : (
+          <>
+            <div className="login__header">
+              <TfiClose
+                className="login__icon"
+                onClick={() => setShowLogin(false)}
+              />
             </div>
+            <div className="login__card">
+              <div className="login__form">
+                <form className="login__email" action="">
+                  <h1 className="login__title">Accede a tu cuenta en Alize</h1>
+                  <div className="login__input-wrapper">
+                    <input
+                      className="login__input"
+                      placeholder="Email"
+                      type="email"
+                    />
+                  </div>
+                  <button className="login__btn">
+                    <span className="login__btn-span">Continuar</span>
+                  </button>
+                </form>
+                <div className="login__middle"></div>
+                <div className="login__access">
+                  <button className="login__access-btn">
+                    <span className="login__access-icon login__access-icon--google"></span>
+                    <p className="login__access-p">Continuar con Google</p>
+                  </button>
+                  <button className="login__access-btn login__access-btn--apple">
+                    <span className="login__access-icon login__access-icon--apple"></span>
+                    <p className="login__access-p">Continuar con Apple</p>
+                  </button>
+                  <button className="login__access-btn">
+                    <span className="login__access-icon login__access-icon--facebook"></span>
+                    <p className="login__access-p">Continuar con Facebook</p>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+        <a className="login__access-password" href="#" onClick={() => setIsRegister(isRegister ? false : true)}>
+          {isRegister
+            ? "Ya tengo cuenta en fotocasa"
+            : "No tengo cuenta y quiero registrarme"}
+        </a>
+      </div>
+    </div>
+  );
+};
+
+const RegisterComponent = ({ setShowLogin }) => {
+  return (
+    <>
+      <div className="login__header">
+        <TfiClose className="login__icon" onClick={() => setShowLogin(false)} />
+      </div>
+      <div className="login__card">
+        <div className="login__form">
+          <form className="login__email" action="">
+            <h1 className="login__title">Crea tu cuenta en Alize</h1>
+            <div className="login__input-wrapper">
+              <input
+                className="login__input"
+                placeholder="Email"
+                type="email"
+              />
+            </div>
+            <button className="login__btn">
+              <span className="login__btn-span">Continuar</span>
+            </button>
+          </form>
+          <div className="login__middle"></div>
+          <div className="login__access">
+            <button className="login__access-btn">
+              <span className="login__access-icon login__access-icon--google"></span>
+              <p className="login__acccess-p">Continuar con Google</p>
+            </button>
+            <button className="login__access-btn login__access-btn--apple">
+              <span className="login__access-icon login__access-icon--apple"></span>
+              <p className="login__acccess-p">Continuar con Apple</p>
+            </button>
+            <button className="login__access-btn">
+              <span className="login__access-icon login__access-icon--facebook"></span>
+              <p className="login__acccess-p">Continuar con Facebook</p>
+            </button>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };

60cc881c7d6b6b0217abe98db13bbcf84cb9ca12
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2023-02-28 18:03:21 -0300

    remove temp view soport

--- src/Views/index.js
+++ src/Views/index.js
@@ -1,8 +1,5 @@
 export { Beneficios } from "./Beneficios/Beneficios";
 export { Destino } from "./Destinos/Destino";
 export { Home } from "./Home/Home";
-export { Soporte } from "./Soporte/Soporte";
 export { Alquiler } from "./Alquiler/Alquiler";
-export { Devolucion } from "./Devolucion/Devolucion";
 export { Info } from "./Info/Info";
-export { Club } from "./Club/Club";

8b4b163e5a2a2aa33261c9cfd6508f472121339c
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2023-02-28 18:14:35 -0300

    add club component

--- src/Views/index.js
+++ src/Views/index.js
@@ -4,3 +4,4 @@ export { Destino } from "./Destinos/Destino";
 export { Home } from "./Home/Home";
 export { Alquiler } from "./Alquiler/Alquiler";
 export { Info } from "./Info/Info";
+export { Club } from "./Club/Club";

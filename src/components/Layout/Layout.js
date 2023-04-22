c611bf8c4572397a4776a0b66cbd0eba935e8af3
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2023-04-21 22:35:58 -0300

    add-layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,7 +5,7 @@ import { Footer } from "../Footer/Footer";
 import { Header } from "../Header/Header";
 
 import { SideMenu } from "./SideMenu";
-import { Favorites } from "../Favorites/Favorites";
+
 import { Button } from '@chakra-ui/react';
 import { RiWhatsappLine } from "react-icons/ri";
 
@@ -18,17 +18,17 @@ export const Layout = ({ children }) => {
 
   return (
     <>
-      <Button className="btn__wpp" position="fixed" zIndex="10" bottom="20px" right="20px" onClick={() => window.open("https://api.whatsapp.com/send?phone=543794077764&text=hola")} height="65px" borderRadius="30px" colorScheme='whatsapp'>
 
+      <Button className="btn__wpp" position="fixed" zIndex="10" bottom="20px" right="20px" onClick={() => window.open("https://api.whatsapp.com/send?phone=543794077764&text=¡Bienvenido Pasajero!. ¿¡En que podemos ayudarte!?")} height="65px" borderRadius="30px" colorScheme='whatsapp'>
         <RiWhatsappLine size={32} />
-
       </Button>
       <Header setIsOpenMenu={setIsOpenMenu} setShowLogin={setShowLogin} setIsFavorite={setIsFavorite} />
       <SideMenu setIsOpenMenu={setIsOpenMenu} isOpenMenu={isOpenMenu} />
       {children}
-      {/* <Login showLogin={showLogin} setShowLogin={setShowLogin} /> */}
-      {/* <Favorites isFavorite={isFavorite} setIsFavorite={setIsFavorite} /> */}
+
       <Footer />
     </>
   );
 };
+
+
